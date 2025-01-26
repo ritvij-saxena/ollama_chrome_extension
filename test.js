c2e2eff69b8e02efd20591c75a09ca402d3d8203
ritvij-saxena <saxenaritvij@gmail.com>
2025-01-25 23:31:23 -0500

    removing test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-const response = await fetch("http://localhost:11434/api/tags");
-
-if (response.ok) {
-  // Parse the response body as JSON
-  const responseBody = await response.json();
-  console.log(responseBody);
-} else {
-  console.log("Error fetching tags:", response.statusText);
-}
